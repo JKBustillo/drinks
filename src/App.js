55542f28e45f6aa6c014d45ea110d9dc51de7369
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-10 23:32:20 -0500

    Context, Form and API call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 
+import CategoriesProvider from './context/CategoriesContext';
+
 function App() {
   return (
-    <Fragment>
+    <CategoriesProvider>
       <Header />
 
       <div className="container mt-5">
@@ -12,7 +14,7 @@ function App() {
           <Form />
         </div>
       </div>
-    </Fragment>
+    </CategoriesProvider>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CategoriesContext } from '../context/CategoriesContext';
 
 const Form = () => {
+    const { categories } = useContext(CategoriesContext);
+
     return (
-        <h1>Form</h1>
+        <form className="col-12">
+            <fieldset className="text-center">
+                <legend>Search for Drinks by Category or Ingredient</legend>
+            </fieldset>
+
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        name="name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Search by Ingredient"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        name="category"
+                        className="form-control"
+                    >
+                        <option value="">-- Select category --</option>
+                        {categories.map(category => (
+                            <option
+                                key={category.strCategory}
+                                value={category.strCategory}
+                            >
+                                {category.strCategory}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Search for drinks"
+                    />
+                </div>
+            </div>
+        </form>
     );
 }
  
